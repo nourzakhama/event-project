8b39b1d0b4ba2ea1d908f249f9081603364f1b0e
nourzakhama <zakhamanoursallem@isimsf.u-sfax.tn>
2025-01-30 14:53:24 +0100

    ra7 l8ali

--- lib/actions/participant.ts
+++ lib/actions/participant.ts
@@ -1,7 +1,7 @@
 import { Participant } from "@/types";
 import axios from "axios";
 
-export const  getParticipants=async()=>{
+export const getParticipants = async () => {
     try {
         const response = await axios.get('http://localhost:4000/participants');
         return response.data;
@@ -11,31 +11,33 @@ export const  getParticipants=async()=>{
     }
 
 }
-export const addParticipant=async(participant:Participant)=>{
-    console.log(participant);
-    try{let res=axios.post('http://localhost:4000/participants',participant);
 
-    }catch(error){
-        console.error('Error adding participant:', error);
-        throw error;
-    }
-
-}
-export const deleteParticipant=async(id:string|undefined)=>{
-    try{
-        let res=axios.delete(`http://localhost:4000/participants/${id}`);
-    }catch(error){
+export const deleteParticipant = async (id: string | undefined) => {
+    try {
+        let res = axios.delete(`http://localhost:4000/participants/${id}`);
+    } catch (error) {
         console.error('Error deleting participant:', error);
         throw error;
     }
 
 }
-export const updateParticipant=async(partId:string,PartData:any)=>{
-    try{
-        axios.patch(`http://localhost:4000/participants/${partId}`,PartData);
+export const updateParticipant = async (partId: string, PartData: any) => {
+    try {
+        axios.patch(`http://localhost:4000/participants/${partId}`, PartData);
 
-    }catch(error){
+    } catch (error) {
         console.error('Error updating participant:', error);
+        throw error;
+    }
+
+}
+export const addParticipant = async (participant: Participant) => {
+    console.log(participant);
+    try {
+        let res = axios.post('http://localhost:4000/participants', participant);
+
+    } catch (error) {
+        console.error('Error adding participant:', error);
         throw error;
     }
 
